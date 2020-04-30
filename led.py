e11ddf63a121f1eda1819981e85111873a2c8d4e
wf <wf@mlf.com>
2020-04-30 23:18:33 +0800

    Signed-off-by: wf <wf@mlf.com> 响应中断

--- led.py
+++ led.py
@@ -3,10 +3,14 @@ import time
  
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.OUT)
- 
-while True:
- 
-    GPIO.output(18, GPIO.HIGH)
-    time.sleep(0.5)
-    GPIO.output(18, GPIO.LOW)
-    time.sleep(0.5)
+try:
+    while True:
+        GPIO.output(18, GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(18, GPIO.LOW)
+        time.sleep(0.5)
+except KeyboardInterrupt as results:
+        print(results)
+finally:
+    print("结束")
+    GPIO.cleanup()
